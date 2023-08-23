973690c5431dd69fd54de1106cf738378b8e6ff5
Mason Austin <mason47austin@gmail.com>
2023-08-22 21:20:32 -0500

    setup for SpellCard

---
+++ components/SpellCard.js
@@ -0,0 +1,11 @@
+import Card from 'react-bootstrap/Card';
+
+function SpellCard() {
+  return (
+    <Card>
+      <Card.Body>This is some text within a card body.</Card.Body>
+    </Card>
+  );
+}
+
+export default SpellCard;
