8bbd9711220a896a3d45dd78dd497e08489e7027
Mason Austin <mason47austin@gmail.com>
2023-08-22 19:15:37 -0500

    finished updateCharacter

--- api/characterData.js
+++ api/characterData.js
@@ -46,11 +46,24 @@ const getSingleCharacter = (firebaseKey) => new Promise((resolve, reject) => {
 });
 
 const deleteSingleCharacter = (firebaseKey) => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/Characters/${firebaseKey}.json`, {
+  fetch(`${endpoint}/characters/${firebaseKey}.json`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateCharacter = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/characters/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
   })
     .then((response) => response.json())
     .then((data) => resolve(data))
@@ -62,4 +75,5 @@ export {
   createCharacter,
   getSingleCharacter,
   deleteSingleCharacter,
+  updateCharacter,
 };
