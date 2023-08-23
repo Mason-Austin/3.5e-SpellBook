7dac9df49ed3c77344e56f1786a62ee8b7b45199
Mason Austin <mason47austin@gmail.com>
2023-08-22 19:14:47 -0500

    finished deleteSingleCharacter

--- api/characterData.js
+++ api/characterData.js
@@ -45,8 +45,21 @@ const getSingleCharacter = (firebaseKey) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const deleteSingleCharacter = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/Characters/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
 export {
   getCharacters,
   createCharacter,
   getSingleCharacter,
+  deleteSingleCharacter,
 };
