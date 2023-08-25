a778146a7cf4afd4c223c427775b07fb358eeb1b
Mason Austin <mason47austin@gmail.com>
2023-08-25 08:32:51 -0500

    finished fucionality of filter

--- components/Search.js
+++ components/Search.js
@@ -6,13 +6,27 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
+const initalState = {
+  name: '',
+  school: '',
+  level: '',
+};
+
 function Search({ contents, setSearchResults }) {
   const [show, setShow] = useState(false);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(initalState);
 
   const handleSubmit = (e) => e.preventDefualt();
 
-  const handleSearchChange = (e) => setInput(e.target.value.toLowerCase());
+  // const handleSearchChange = (e) => setInput(e.target.value.toLowerCase());
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((preveState) => ({
+      ...preveState,
+      [name]: value,
+    }));
+  };
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -21,7 +35,41 @@ function Search({ contents, setSearchResults }) {
     if (!contents) {
       setSearchResults(contents);
     }
-    const resultsArray = contents?.filter((content) => content.name.toLowerCase().includes(input));
+    let resultsArray = [];
+    if (!input.name && !input.school && !input.level) {
+      resultsArray = contents;
+    } else if (input.name && input.school && input.level) {
+      resultsArray = contents?.filter((content) => content.name?.toLowerCase().includes(input.name) && content.school.toLowerCase().includes(input.school) && content.level?.toLowerCase().includes(input.level));
+    } else if (input.name && input.school) {
+      resultsArray = contents?.filter((content) => content.name?.toLowerCase().includes(input.name) && content.school.toLowerCase().includes(input.school));
+    } else if (input.name && input.level) {
+      resultsArray = contents?.filter((content) => content.name?.toLowerCase().includes(input.name) && content.level.toLowerCase().includes(input.level));
+    } else if (input.school && input.level) {
+      resultsArray = contents?.filter((content) => content.level?.toLowerCase().includes(input.level) && content.school.toLowerCase().includes(input.school));
+    } else if (input.name) {
+      resultsArray = contents?.filter((content) => content.name?.toLowerCase().includes(input.name));
+    } else if (input.school) {
+      resultsArray = contents?.filter((content) => content.school?.toLowerCase().includes(input.school));
+    } else if (input.level) {
+      resultsArray = contents?.filter((content) => content.level?.toLowerCase().includes(input.level));
+    }
+    // switch (input) {
+    //   case input.name:
+    //     console.warn('name');
+    //     break;
+
+    //   case input.school:
+    //     console.warn('school');
+    //     break;
+
+    //   case input.level:
+    //     console.warn('level');
+    //     break;
+
+    //   default:
+    //     console.warn('no filter');
+    //     break;
+    // }
     setSearchResults(resultsArray);
   }, [input]);
 
@@ -40,18 +88,31 @@ function Search({ contents, setSearchResults }) {
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={handleSubmit}>
-            <Form.Group>
-              <Form.Control
-                type="text"
-                onKeyUp={handleSearchChange}
-                placeholder="Search here"
-                className="search"
-              />
-            </Form.Group>
+            <Form.Control
+              type="text"
+              onChange={handleChange}
+              placeholder="Spell Name"
+              name="name"
+              value={input.name}
+            />
+            <Form.Control
+              type="text"
+              onChange={handleChange}
+              placeholder="School of Magic"
+              name="school"
+              value={input.school}
+            />
+            <Form.Control
+              type="text"
+              onChange={handleChange}
+              placeholder="Spell Level"
+              name="level"
+              value={input.level}
+            />
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={() => { setSearchResults(contents); }}>
+          <Button variant="secondary" onClick={() => { setInput(initalState); }}>
             Clear Filter
           </Button>
           <Button variant="secondary" onClick={handleClose}>
