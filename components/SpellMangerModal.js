2c29cb88bfd02a83459d08f720d26612823233ff
Mason Austin <mason47austin@gmail.com>
2023-09-06 20:35:06 -0500

    added feature to allow user to increase or decrease the amout of spell slots they have with respect to the max and min spell slots

--- components/SpellMangerModal.js
+++ components/SpellMangerModal.js
@@ -1,16 +1,44 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import PropTypes from 'prop-types';
 import SpellSlot from './SpellSlot';
+import { updateCharacter } from '../api/characterData';
 
 function SpellManagerModal({ characterObj }) {
   const [show, setShow] = useState(false);
+  const [spellSlots, setSpellSlots] = useState([]);
 
+  useEffect(() => {
+    setSpellSlots(characterObj.spell_slots);
+  }, [characterObj]);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const handleClickPlus = (arryIndex) => {
+    console.warn('plus is working');
+    if (spellSlots[arryIndex] === characterObj.max_spell_slots[arryIndex]) {
+      window.alert('Spell slots can not exceed your max limit');
+    } else {
+      const updatedSpellSlots = [...spellSlots];
+      updatedSpellSlots[arryIndex] += 1;
+      setSpellSlots(updatedSpellSlots);
+    }
+  };
+  const handleSave = () => {
+    updateCharacter({ ...characterObj, spell_slots: spellSlots });
+    handleClose();
+  };
+  const handleClickMinus = (index) => {
+    if (spellSlots[index] === 0) {
+      window.alert('Spell slots can not go below 0');
+    } else {
+      const updatedSpellSlots = [...spellSlots];
+      updatedSpellSlots[index] -= 1;
+      setSpellSlots(updatedSpellSlots);
+    }
+  };
   return (
     <>
       {/* <Button variant="primary" onClick={handleShow}>
@@ -24,15 +52,16 @@ function SpellManagerModal({ characterObj }) {
         onHide={handleClose}
         backdrop="static"
         keyboard={false}
+        size="lg"
         style={{ color: 'black' }}
       >
         <Modal.Header closeButton>
           <Modal.Title>Spell Manager</Modal.Title>
         </Modal.Header>
         <Modal.Body className="rw">
-          {characterObj.spell_slots?.map((spellLevel, index) => (
+          {spellSlots?.map((spellLevel, index) => (
             spellLevel !== null ? (
-              <SpellSlot currentSpellSlot={spellLevel} maxSpellSlot={characterObj.max_spell_slots[index]} />
+              <SpellSlot currentSpellSlot={spellLevel} maxSpellSlot={characterObj.max_spell_slots[index]} spellLevel={index} handleClickMinus={() => handleClickMinus(index)} handleClickPlus={() => handleClickPlus(index)} />
             ) : null
           ))}
         </Modal.Body>
@@ -40,7 +69,7 @@ function SpellManagerModal({ characterObj }) {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Understood</Button>
+          <Button variant="primary" onClick={handleSave}>Understood</Button>
         </Modal.Footer>
       </Modal>
     </>
--- components/SpellSlot.js
+++ components/SpellSlot.js
@@ -1,16 +1,33 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { CircularProgress } from '@mui/material';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import { FaPlus, FaMinus } from 'react-icons/fa';
 
-export default function SpellSlot({ currentSpellSlot, maxSpellSlot }) {
+export default function SpellSlot({
+  currentSpellSlot, maxSpellSlot, spellLevel, handleClickPlus, handleClickMinus,
+}) {
+  const [spellPrecentage, setSpellPrecentage] = useState();
+  useEffect(() => {
+    setSpellPrecentage((currentSpellSlot / maxSpellSlot) * 100);
+  }, [currentSpellSlot]);
   return (
     <>
       <div style={{ border: 'red dotted 1px', position: 'relative' }}>
-        <p style={{ position: 'absolute', right: '20%', top: '15%' }}>{currentSpellSlot}/{maxSpellSlot}</p>
-        <FaMinus />
-        <CircularProgress variant="determinate" value={50} />
-        <FaPlus />
+        <p style={{ margin: 'auto', textAlign: 'center' }}>Level {spellLevel} </p>
+        <p style={{ position: 'absolute', right: '33%', bottom: '0%' }}>{currentSpellSlot}/{maxSpellSlot}</p>
+        <FaMinus onClick={handleClickMinus} />
+        <CircularProgress variant="determinate" value={spellPrecentage} />
+        <FaPlus onClick={handleClickPlus} />
       </div>
     </>
   );
 }
+
+SpellSlot.propTypes = {
+  currentSpellSlot: PropTypes.number,
+  maxSpellSlot: PropTypes.number,
+  spellLevel: PropTypes.number,
+  handleClickMinus: PropTypes.func,
+  handleClickPlus: PropTypes.func,
+}.isRequired;
