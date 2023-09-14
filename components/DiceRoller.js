3096243182614cc9a1e5f1b070b24f6f369f92ea
Mason Austin <mason47austin@gmail.com>
2023-09-13 23:11:03 -0500

    added physcis based dice roller

---
+++ components/DiceRoller.js
@@ -0,0 +1,35 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import DiceBox from '@3d-dice/dice-box';
+import { Button } from 'react-bootstrap';
+
+function DiceComponent() {
+  const [newDiceBox, setNewDiceBox] = useState();
+  const handleDiceRoll = () => {
+    newDiceBox.roll('1d20');
+  };
+  useEffect(() => {
+    // Create a new instance of DiceBox and initialize it when the component mounts
+    const diceBox = new DiceBox('#dice-box', {
+      id: 'dice-canvas',
+      assetPath: '/assets/dice-box/', // Adjust the path based on your project structure
+      scale: 9,
+      throwForce: 9,
+    });
+
+    setNewDiceBox(diceBox);
+
+    diceBox.init();
+
+    // Clean up the DiceBox instance when the component unmounts
+  }, []);
+
+  return (
+    <>
+      <Button type="button" variant="danger" onClick={handleDiceRoll}>Sign Out</Button>
+      <div id="dice-box" />
+    </>
+  );
+}
+
+export default DiceComponent;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect, useState } from 'react';
+// import DiceBox from '@3d-dice/dice-box'; // Import the DiceBox module
 import CharacterCard from '../components/CharacterCard';
+import DiceRoller from '../components/DiceRoller';
 import { useAuth } from '../utils/context/authContext';
 import { getCharacters } from '../api/characterData';
 
 function Home() {
   const [characters, setCharacters] = useState([]);
-
   const { user } = useAuth();
 
   const getAllCharacters = () => {
@@ -18,15 +19,33 @@ function Home() {
     getAllCharacters();
   }, []);
 
+  // useEffect(() => {
+  //   // Create a new instance of DiceBox and initialize it when the component mounts
+  //   const diceBox = new DiceBox('#dice-box', {
+  //     id: 'dice-canvas',
+  //     assetPath: '/assets/dice-box/', // Adjust the path based on your project structure
+  //     scale: 9,
+  //     throwForce: 9,
+  //   });
+
+  //   diceBox.init().then(() => {
+  //     // Now the diceBox is ready to roll some dice
+  //     diceBox.roll('5d4');
+  //   });
+  // }, []);
+
   return (
-    <div style={{ margin: 'auto auto', width: '100%' }}>
-      <h1>Characters</h1>
-      <div className="rw">
-        {characters.map((character) => (
-          <CharacterCard key={character.firebaseKey} characterObj={character} onUpdate={getAllCharacters} />
-        ))}
+    <>
+      <div style={{ margin: 'auto auto', width: '100%' }}>
+        <h1>Characters</h1>
+        <div className="rw">
+          {characters.map((character) => (
+            <CharacterCard key={character.firebaseKey} characterObj={character} onUpdate={getAllCharacters} />
+          ))}
+        </div>
+        <DiceRoller />
       </div>
-    </div>
+    </>
   );
 }
 
